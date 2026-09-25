02776026a66495d02ad78cdd91184dd0d056cecf
Punsara Bandara <punsarab@unleashideas.com>
2026-09-25 18:28:05 +0530

    docker-compose.yml removed, hello synlio server msg added.

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Synlio server is up!';
+    return 'hello synlio server!';
   }
 }
